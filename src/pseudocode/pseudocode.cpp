d2ef3140c98b08408b615cbf48346563d9e50e28
Benoit Cotte <benoit.cotte@essec.edu>
2014-06-13 16:27:50 +0200

    Add sendJson() and clean serial

--- src/pseudocode/pseudocode.cpp
+++ src/pseudocode/pseudocode.cpp
@@ -7,13 +7,14 @@
 #include <Arduino.h>
 #include "SteamSensor.h"
 #include "Led.h"
+#include <string.h>
 
 //Object
 Led led = Led(11, 12, 13);
 SteamSensor steamsensor;
 
 // PROTOTYPES
-void sendAverage();
+void sendJson(uint8_t z, uint16_t a);
 
 //Variables
 uint16_t steamSensorMin = 1023;        // minimum sensor value before calibration
@@ -24,6 +25,7 @@ uint8_t zone;			// declare zones
 uint16_t average[6]; 	// declare the array
 unsigned long startTime;
 unsigned long stopTime;
+String json;
 
 void setup ()	{
 
@@ -50,15 +52,15 @@ void setup ()	{
     	// record the maximum sensor value
 
       	steamSensorMax = 125;		// Determined off calibration
-  		Serial.print("Max is ");
-  		Serial.println(steamSensorMax);
+  		// Serial.print("Max is ");
+  		// Serial.println(steamSensorMax);
 
     	// record the minimum sensor value
     	if (steamsensor.getValue() < steamSensorMin) 
       	{
       		steamSensorMin = steamsensor.getValue();	// Determined during calibration
-      		Serial.print("Min is ");
-      		Serial.println(steamSensorMin);
+      		// Serial.print("Min is ");
+      		// Serial.println(steamSensorMin);
   		}
   	}
 
@@ -87,14 +89,14 @@ void loop ()	{
 		
 		steamsensor.readValue();		// Updated mesure to enter the loop
 
-		while(steamsensor.getValue() > 0)		// Place sensor on zone i
+		while(steamsensor.getValue() > steamSensorMin)		// Place sensor on zone i
 		{
 			steamsensor.readValue(); 	// Read and add it to sum
-			steamsensor.debug();	
+			// steamsensor.debug();	
 			sum += steamsensor.getValue();
 			
-			Serial.print("sum is ");
-			Serial.println(sum);
+			// Serial.print("sum is ");
+			// Serial.println(sum);
 
 			led.blink();						// Blink during measure
 			nbrValue += 1;						// Increment the number of measures
@@ -104,24 +106,32 @@ void loop ()	{
 		
 		stopTime = millis();			// When value is back to 0 ==> stop the timer
 		
-		Serial.print("time is ");
-		Serial.println(stopTime - startTime);
+		// Serial.print("time is ");
+		// Serial.println(stopTime - startTime);
 		
 		if(stopTime - startTime >= 2000)		// If time is over 2 seconds
 		{
-			Serial.print("sum is ");
-			Serial.println(sum);
+			// Serial.print("sum is ");
+			// Serial.println(sum);
 
-			Serial.print("nbrValue is ");
-			Serial.println(nbrValue);				
+			// Serial.print("nbrValue is ");
+			// Serial.println(nbrValue);				
 
 			average[zone] = map(sum / nbrValue, steamSensorMin, steamSensorMax, 0, 100);	// compute the average
-			sendAverage();		// send the average 
-			
-			Serial.print("average zone ");
-			Serial.print(zone);
-			Serial.print(" is ");
-			Serial.println(average[zone]);
+
+			// json = "[{zone:";
+			// json += zone;
+			// json += ",avg:";
+			// json += average[zone];
+			// json += "}]";
+			// Serial.println(json);
+
+			sendJson(zone, average[zone]);
+
+			// Serial.print("average zone ");
+			// Serial.print(zone);
+			// Serial.print(" is ");
+			// Serial.println(average[zone]);
 
 			zone = (zone + 1)%6 ;
 		} 
@@ -131,10 +141,14 @@ void loop ()	{
 
 }
 
-void sendAverage()	{
-	/*
-	*	TO BE IMPLEMENTED
-	*/
+void sendJson(uint8_t z, uint16_t a)
+{
+	json = "[{zone:";
+	json += z;
+	json += ",avg:";
+	json += a;
+	json += "}]";
+	Serial.println(json);
 }
 	
 		
